aeed4d59f796f854cc90742c0124b80a96ee4beb
nateYourNanny <nateliu@tw.ibm.com>
2019-01-02 14:18:26 +0800

    update readme

--- src/tic-tac-toe.test.js
+++ src/tic-tac-toe.test.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Adapter from 'enzyme-adapter-react-16'
-import { configure, shallow, mount } from 'enzyme'
-configure({ adapter: new Adapter() })
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, mount } from 'enzyme';
 
-import { Game, Board, Square } from './tic-tac-toe'
+configure({adapter: new Adapter()});
+
+import { Game, Board, Square } from './tic-tac-toe';
 
 describe('<App /> shallow rendering', () => {
   it('h1 contains correct text', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.find('h1').text()).toBe('Welcome to React')
-  })
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('h1').text()).toBe('Welcome to React');
+  });
+});
